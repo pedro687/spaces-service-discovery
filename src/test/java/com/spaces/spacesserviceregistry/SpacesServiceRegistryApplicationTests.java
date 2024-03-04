a89d60c045b5b1b1810618be63e7d5208be4a9e8
pedro687 <tyroxbr@gmail.com>
2024-03-04 05:03:04 -0300

    feat: creating service registry

---
+++ src/test/java/com/spaces/spacesserviceregistry/SpacesServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaces.spacesserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacesServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
